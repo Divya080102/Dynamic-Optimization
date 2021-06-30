5a8a60db02fe93cd49c384781962d465d68f8dd4
Divya080102 <85280323+Divya080102@users.noreply.github.com>
2021-06-30 16:30:56 +0530

    Update MyReactor_final2.jl

--- MyReactor_final2.jl
+++ MyReactor_final2.jl
@@ -5,16 +5,17 @@ using Fresa                     # for optimization
 using Printf                    # for formatted output
 using PyPlot                    # for plotting profiles
 
-const tfinal = 390.0         # hours
-const T = 303.0              # Kelvin
-const pH = 7.0
-const F = 0.5
-const Vmax = 100.0
-const Vmin = 60.0
+#constant conditions of the reactor - 
+const tfinal = 390.0		# hours
+const T = 303.0			# Kelvin
+const pH = 7.0			#Value of pH for the reactions
+const F = 0.5			#feedrate
+const Vmax = 100.0		#Maximum volume of contents of the reactor(= volume of the reactor container)
+const Vmin = 60.0		#Initial volume of the reactor
 
-mutable struct PiecewiseLinearProfile
+mutable struct PiecewiseLinearProfile	#Structure to create profile for time spent in the batch reactor
     ft :: Array{Float64}        # time deltas
-    function PiecewiseLinearProfile(ft)
+    function PiecewiseLinearProfile(ft)	
 	new(ft)
     end
     function PiecewiseLinearProfile(t :: PiecewiseLinearProfile)
@@ -22,10 +23,10 @@ mutable struct PiecewiseLinearProfile
     end
 end
 
-function volume(profile :: PiecewiseLinearProfile, t :: Float64) :: Float64
-  #  τsum = 0
+function volume(profile :: PiecewiseLinearProfile, t :: Float64) :: Float64	#Finding volume at a particular time point
+										#Rate of volume change = Feedrate(F) = 0.5
     τ = 0
-    V = Vmin
+    V = Vmin	
     for i=1:length(profile.ft)
         Δτ = profile.ft[i] * (tfinal - τ)
 	ΔV = F*Δτ
@@ -55,19 +56,20 @@ function volume(profile :: PiecewiseLinearProfile, t :: Float64) :: Float64
     return V
 end
 
-function reactor(delements, elements :: Array{Float64},
-		 p :: PiecewiseLinearProfile,
+#function to formulate the mathematical equations of the paper
+function reactor(delements, elements :: Array{Float64}, 	#the array elements contains values of X(microbial growth concentration), S(concentration of substrate), P(concentration of Product) respectively
+		 p :: PiecewiseLinearProfile,			
 		 tf:: Float64
                  ) 
    # t = time(p,tf)
-    V = volume(p,tf)
+    V = volume(p,tf)		#finding the volume of reactor contents at a given time tf
  #   println("Volume = $V")
-    CO = 0.037
+    CO = 0.037		#constant CO concentration in the reactor
     # println("$t")
     X, S, P = elements
     delements[1] = ((5.53537543*(10^-12)*(S/(S + 0.1828*X))*(CO/(0.0352*X + CO))*(1-(X/0.87))) - (4.60796599*(10^-62)*(1-(CO/(0.0368+CO)))) - F/V)*X
     delements[2] = -0.0624*X - 4*(((5.53537543*(10^-12)*(S/(S + 0.1828*X))*(CO/(0.0352*X + CO))*(1-(X/0.87))) - (4.60796599*(10^-62)*(1-(CO/(0.0368+CO)))) - F/V)*X) - ((1/0.68)* 0.05*S*X/(0.0002+S+(10*S^2)) - 4*10^(-4)*P - F*P/V) - F*X/V
-    delements[3] =  0.05*S*X/(0.0002+S+(10*S^2)) - 4*10^(-4)*P - F*P/V
+    delements[3] =  0.05*S*X/(0.0002+S+(10*S^2)) - 4*10^(-4)*P - F*P/V			#created the ODE equations 
   #  delements[4] =  F	
  #   print("X = $X, S = $S, P = $P")
    # return delements
